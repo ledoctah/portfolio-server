27051a140d7a026e159ff0f5773a169db243ba5e
ledoctah <matheusferr2015@gmail.com>
2020-02-16 21:41:59 -0300

    Added template engine (.njk )+ 404 error handling

--- server.js
+++ server.js
@@ -5,19 +5,23 @@ const server = express();
 
 server.use(express.static('public'));
 
-server.set('view engine', 'html');
+server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
     express: server
 });
 
 server.get('/', function(req, res) {
-    return res.render('index');
+    return res.render('about');
 });
 
 server.get('/portfolio', function(req, res){
     return res.render('portfolio');
 }),
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+});
 
 server.listen(5000, function() {
     console.log('Server is running');
